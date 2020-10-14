141ef51285e97fc3b1e73a375a5291178d02b4c0
Mark Pavlovski <markski@amazon.com>
2020-10-14 13:03:36 -0700

    added basic nav

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -2,26 +2,28 @@ import React from "react";
 import {LayoutProps} from "./types";
 import styles from './Layout.module.scss'
 import cn from "classnames";
+import Nav from "../nav/Nav";
 
 const Layout: React.FC<LayoutProps> = (props) => {
   const {children, debug} = props;
   return (
     <div className={styles.wallToWallContainer}>
-      <header className={styles.outerContainerHeader}>
+      <header className={cn(styles.outerContainerHeader, styles.stickyHeader)}>
         <div className={styles.innerContainerHeader}>
-          Hello
+          <Nav/>
         </div>
       </header>
-      <div className={cn(styles.outerContainer, {
+      <div className={cn(styles.outerContainerBody, {
         [styles.debug]: debug,
       })}>
-        <div className={cn(styles.innerContainer, {
+        <div className={cn(styles.innerContainerBody, {
           [styles.debug]: debug,
         })}>
           {children}
         </div>
       </div>
-    </div>)
+    </div>
+  )
 };
 
 export default Layout;
---
+++ components/nav/Nav.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from './Nav.module.scss'
+import cn from "classnames";
+
+const Nav: React.FC = () => {
+  return (
+    <div className={styles.navContainer}>
+      Hi
+    </div>
+  )
+};
+
+export default Nav;
